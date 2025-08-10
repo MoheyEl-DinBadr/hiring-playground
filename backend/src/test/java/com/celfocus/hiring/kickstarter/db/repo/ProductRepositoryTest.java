b93f068ee75fad04d7ed7d730c0c72fb26cdac75
MoheyElDin Badr <MoheyElDin.Badr@gmail.com>
2025-08-11 00:04:09 +0300

    Update ProductRepositoryTest to enable Spring Data JPA testing with constructor injection

--- backend/src/test/java/com/celfocus/hiring/kickstarter/db/repo/ProductRepositoryTest.java
+++ backend/src/test/java/com/celfocus/hiring/kickstarter/db/repo/ProductRepositoryTest.java
@@ -2,19 +2,21 @@ package com.celfocus.hiring.kickstarter.db.repo;
 
 import com.celfocus.hiring.kickstarter.db.entity.ProductEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@DataJpaTest
 class ProductRepositoryTest {
 
 
     private final ProductRepository productRepository;
 
-    public ProductRepositoryTest(ProductRepository productRepository) {
+    public ProductRepositoryTest(@Autowired ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
